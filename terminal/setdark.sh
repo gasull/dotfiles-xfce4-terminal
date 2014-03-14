c2fa1b88ef1e8bf6fa7673768c0fa2950219ea08
Daniel Gonzalez Gasull <gasull@gmail.com>
2014-03-14 06:42:17 -0700

    Add dark/light terminalrcs and scripts

---
+++ terminal/setdark.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Set dark solarized theme
+cd ~/.config/xfce4/terminal
+cp -f dark.terminalrc terminalrc
---
+++ terminal/setlight.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Set light solarized theme
+cd ~/.config/xfce4/terminal
+cp -f light.terminalrc terminalrc
